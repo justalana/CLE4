5f278a9a93bccde93d47b13acea32db226d4994c
hugodeman <145498441+hugodeman@users.noreply.github.com>
2024-06-24 10:33:32 +0200

    Update levelfail.js

--- src/js/levelfail.js
+++ src/js/levelfail.js
@@ -22,35 +22,11 @@ export class LevelFail extends Scene {
             })
         })
         engine.add(this.title)
-
-        this.home = new Label({
-            text: `Return to Boat`,
-            pos: new Vector(240, 340),
-            font: new Font({
-                family: 'impact',
-                size: 40,
-                unit: FontUnit.Px,
-                color: Color.Black
-            })
-        })
-        engine.add(this.home)
-
-        this.retry = new Label({
-            text: `Try again`,
-            pos: new Vector(240, 230),
-            font: new Font({
-                family: 'impact',
-                size: 40,
-                unit: FontUnit.Px,
-                color: Color.Black
-            })
-        })
-        engine.add(this.retry)
     }
 
     createBackground(engine) {
         const bg = new BG()
-        bg.graphics.use(Resources.BGCruise.toSprite())
+        bg.graphics.use(Resources.BGFish.toSprite())
         this.add(bg)
     }
 }
