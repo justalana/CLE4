5ae83865a1e4947e538db02c7fba6ed1f7272a64
hugodeman <145498441+hugodeman@users.noreply.github.com>
2024-06-24 10:35:24 +0200

    Update resources.js

--- src/js/resources.js
+++ src/js/resources.js
@@ -5,6 +5,9 @@ const Resources = {
     BGTaxi: new ImageSource('images/BG-watertaxi.JPG'),
     BGCruise: new ImageSource('images/BG-cruise.JPG'),
     BGHole: new ImageSource('images/BG-hole.JPG'),
+    BGFixed: new ImageSource('images/BG-fix.PNG'),
+    BGFish: new ImageSource('images/BG-vissen.PNG'),
+    BGIntro: new ImageSource('images/BG-intro.PNG'),
     Drill: new ImageSource('images/drill.png'),
     Hammer: new ImageSource('images/hammer.png'),
     Pickaxe: new ImageSource('images/pickaxe.png'),
@@ -17,7 +20,8 @@ const Resources = {
     Whitebar: new ImageSource('images/whitebar.png'),
     Greenbar: new ImageSource('images/greenbar.png'),
     WaterSplash: new Sound('Sounds/splash2.wav'),
-    Pipe: new Sound('Sounds/pipe.mp3')
+    Pipe: new Sound('Sounds/pipe.mp3'),
+    A: new ImageSource('images/A.png')
 }
 
 const ResourceLoader = new Loader()
